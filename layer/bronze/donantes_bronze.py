3bd0f542f77a4dfbafd8c829dc7dbf582a00d52b
GerDomin <germandominguezc@gmail.com>
2025-11-14 20:54:22 -0300

    modificacion

--- layer/bronze/donantes_bronze.py
+++ layer/bronze/donantes_bronze.py
@@ -1,3 +1,3 @@
 # Archivo indicador para la capa Bronze
-# Generado: 2025-11-14T18:45:44.010697+00:00
+# Generado: 2025-11-14T23:48:28.427814+00:00
 # Contiene: donantes_bronze.parquet (datos crudos procesados)
--- layer/silver/donantes_silver.py
+++ layer/silver/donantes_silver.py
@@ -1,3 +1,3 @@
 # Archivo indicador para la capa Silver
-# Generado: 2025-11-14T18:45:53.085020+00:00
+# Generado: 2025-11-14T23:48:36.495520+00:00
 # Contiene: donantes_silver.parquet (datos limpios y pivot mensuales)
